c2e55e31b3828a5e450ff5f2d5c75f4cbe7b1c99
am4847 <am4847@kakao.com>
2021-08-03 22:38:29 +0900

    setting success

---
+++ src/main/java/come/dong/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package come.dong.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController// 스프링 com.###.### 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것은 아니고,
+					// 특정 어노테이션이 붙어있는 클래스 파일들을 new 해서 스프링 컨테이너에 관리해준다.
+public class BlogControllerTest {
+	
+		@GetMapping("/test/hello")
+		public String hello() {
+			return "<h1>hello Spring boot</h1>";
+		}
+}
